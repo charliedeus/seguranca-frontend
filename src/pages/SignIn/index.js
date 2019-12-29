77034f79e80bf43a737e87c30df77d6bb28cfb5b
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-12-29 15:31:57 -0300

    Auth styling

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+// import logo from '~/assets/agerba.png';
 
 export default function SignIn() {
   return (
     <>
-    <h1>Sign In Page</h1>
+      <img src={logo} alt="Go Barber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+// import logo from '~/assets/agerba.png';
 
 export default function SignUp() {
   return (
     <>
-    <h1>Profile Page</h1>
+      <img src={logo} alt="Go Barber" />
+
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/signin">Já tenho login</Link>
+      </form>
     </>
   );
 }
