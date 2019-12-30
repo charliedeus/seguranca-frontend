e3e80e1e9e483e929ccec7de574232adf7774714
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-12-30 14:15:32 -0300

    Authentication user

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,9 +1,20 @@
+import produce from 'immer';
+
 const INITIAL_STATE = {
-  token: '123',
+  token: null,
+  signed: false,
+  active: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+        draft.active = action.payload.user.active;
+      });
     default:
       return state;
   }
