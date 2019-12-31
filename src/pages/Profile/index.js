39a2da5ebb30e10a086a3701165f5b038cef725b
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-12-31 13:26:34 -0300

    Profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
-    <>
-    <h1>Profile Page</h1>
-    </>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua nova senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair</button>
+    </Container>
   );
 }
