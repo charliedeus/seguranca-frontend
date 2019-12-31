1e77aad236ec229dd90697c1cd3112aaabe50eb9
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-12-31 14:04:30 -0300

    Profile page update

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <Input name="cpf" placeholder="CPF" />
 
         <hr />
 
